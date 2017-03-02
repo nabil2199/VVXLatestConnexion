d4b608697ee691fa9ad72625dc788b91f7b322d3
nabil2199 <nabil2199@gmail.com>
2017-03-02 17:09:34 +0100

    File output

--- VVX_latest.ps1
+++ VVX_latest.ps1
@@ -7,7 +7,7 @@ OCWS
 param([string]$userCsv = "C:\Sources\users.csv",[string]$IPfilePath = "C:\Sources\ProvisioningPIN.csv")
 
 #IP file initialise
-Add-Content "upn,FirstName,LastName,EmailAddress,Desk,Extension,PIN" -Path $IPfilePath
+Add-Content "upn,SipAddress,IP_address" -Path $IPfilePath
 
 # SQL Server for historical connection information
 $SqlServer = "dsi-w4173bddv0.groupe.generali.fr\SQLT110_LYNK2_PA"
@@ -24,4 +24,35 @@ foreach ($user in $usersList)
     $UserSIP = $user.SipAddress
     Write-Host " "
     Write-Host "Retrieving Recent Lync Client Connections for " -foregroundcolor green -NoNewline; Write-Host $UserSIP -foregroundcolor blue
-    
+
+    $SQLQuery = "SELECT TOP 1 [SessionIdTime]
+      ,[RegisterTime]
+      ,[UserUri]
+      ,[ClientVersion]
+      ,[IpAddress]
+      ,[DiagnosticId]
+      ,[Registrar]
+      ,[Pool]
+      ,[IsUserServiceAvailable]
+      ,[DeviceMacAddress]
+      ,[DeviceManufacturer]
+      ,[DeviceHardwareVersion]
+      ,[DeRegisterTime]
+      FROM [LcsCDR].[dbo].[RegistrationView] where [UserUri] LIKE '%$UserSIP%' AND [ClientVersion] LIKE '%Polycom%' ORDER BY SessionIdTime DESC"
+      
+    $Connection = new-object system.data.sqlclient.sqlconnection
+    $Connection.connectionString="Data Source=$SqlServer;Initial Catalog=LcsCDR;Integrated Security=SSPI"
+    $Connection.open()
+    $Command = $Connection.CreateCommand()
+    $Command.Commandtext = $SqlQuery
+    $DataAdapter = New-Object System.Data.SqlClient.SqlDataAdapter $Command
+    $Dataset = New-Object System.Data.Dataset
+    $DataAdapter.Fill($Dataset)
+    # $Dataset.Tables[0] | Export-CSV TempList.csv -notype
+    $Connection.close()
+    $Connection = $null
+
+    $Results = $Dataset.Tables[0].rows
+    $line = $user.upn + "," + $UserSIP + "," + $Results.IpAddress
+
+}
